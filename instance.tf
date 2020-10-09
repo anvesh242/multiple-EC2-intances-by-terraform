4e680b7bc7e57c04465edc55b53db6447b4d7a7f
anvesh242 <anvesh.242@gmail.com>
2020-10-08 20:00:00 -0400

    Create instance.tf
    Added insatnce.tf

---
+++ instance.tf
@@ -0,0 +1,23 @@
+
+#How to launch multple EC2 instance by using terraform
+#anvesh@Anvesh:~/opt/terraform$ cat instance.tf
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAJIYEIFI4IGPYD3BQ"
+  secret_key = "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"
+}
+#resource "aws_key_pair" "k8s-demo2" {
+ # key_name   = "k8s-demo2"
+ # public_key = "${file("k8s-demo1.pem")}"
+#}
+resource "aws_instance" "master-instance" {
+  count         = "${var.instance_count}"
+  ami           = "${lookup(var.ami, var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name      = "k8s-demo1"
+  #key_name = "${aws_key_pair.k8s-demo2.key_name}"
+  tags = {
+    Name = "${element(var.instance_tags, count.index)}"
+  }
+}
+anvesh@Anvesh:~/opt/terraform$
